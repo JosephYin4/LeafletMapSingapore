b4fe694eaccfc1432499985e6822108f738d25a3
JosephYin4 <yincc147@hotmail.com>
2024-08-06 17:22:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -28,3 +28,39 @@ circle.addTo(map);
 let zooMarker = L.marker([1.4026483894, 103.788013515]);
 zooMarker.addTo(map);
 zoo.bindPopup("<p>Zoo</p>")
+
+// the map argument refers to the map which we create using Leaflet
+function getRandomLatLng(map) {
+    // get the boundaries of the map
+    let bounds = map.getBounds();
+    let southWest = bounds.getSouthWest();
+    let northEast = bounds.getNorthEast();
+    let lngSpan = northEast.lng - southWest.lng;
+    let latSpan = northEast.lat - southWest.lat;
+
+    let randomLng = Math.random() * lngSpan + southWest.lng;
+    let randomLat = Math.random() * latSpan + southWest.lat;
+
+    return [ randomLat, randomLng,];
+}
+
+let singapore = [ 1.29,103.85]; // Singapore latlng
+let map = L.map('map').setView(singapore, 13);
+
+// setup the tile layers
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw' //demo access token
+}).addTo(map);
+
+// create marker cluster
+let markerClusterLayer = L.markerClusterGroup();
+
+for (let i = 0; i < 1000; i++) {
+    let pos = getRandomLatLng(map);
+    L.marker(pos).addTo(markerClusterLayer);
+}
